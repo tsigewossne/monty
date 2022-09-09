26390cb7e5822e76757053c1de2812eb3c56599a
tsigewossne <tsigedejen@gmail.com>
2022-09-09 10:33:51 +0300

    push,pall

--- 0-push_pall.c
+++ 0-push_pall.c
@@ -1,57 +1,87 @@
 #include "monty.h"
-
+#include "lists.h"
 /**
- * _push - push a element to the stack
- * @stack: double pointer to the satack
- * @line_number: line number
- *
- * Return: nothing
+ * get_func - selects the right function
+ * @parsed: line from the bytecode file passed to main
+ * Return: pointer to the selected function, or NULL on failure
  */
-void _push(stack_t **stack, unsigned int line_number)
+void (*get_func(char **parsed))(stack_t **, unsigned int)
 {
-/*char *arg = strtok(NULL, " \n\t\r");*/
-char *arg = e_vars.op_arg;
-int i;
-stack_t *tmp;
-if (arg != NULL)
+instruction_t func_arr[] = {
+			      {"push", push_handler},
+			      {"pall", pall_handler},
+			      {"pint", pint_handler},
+			      {"pop", pop_handler},
+			      {"swap", swap_handler},
+			      {"add", add_handler},
+			      {"nop", nop_handler},
+			      {"sub", sub_handler},
+			      {"div", div_handler},
+			      {"mul", mul_handler},
+			      {"mod", mod_handler},
+			      {"pchar", pchar_handler},
+			      {"pstr", pstr_handler},
+			      {"rotl", rotl_handler},
+			      {"rotr", rotr_handler},
+			      {"stack", stack_handler},
+			      {"queue", queue_handler},
+			      {NULL, NULL}
+};
+int codes = 17, i;
+for (i = 0; i < codes; i++)
 {
-for (i = 0; arg[i]; i++)
+if (strcmp(func_arr[i].opcode, parsed[0]) == 0)
 {
-if (arg[0] == '-')
-continue;
-if (isdigit(arg[i]) == 0)
-break;
+return (func_arr[i].f);
 }
-if (arg[i] == '\0')
+}
+return (NULL);
+}
+/**
+ * push_handler - handles the push instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
+ */
+void push_handler(stack_t **stack, unsigned int line_number)
 {
-i = atoi(arg);
-tmp = add_snode(stack, i);
-if (tmp == NULL)
+stack_t *new;
+int num = 0, i;
+if (data.words[1] == NULL)
 {
-dprintf(STDERR_FILENO, "Error: malloc failed\n");
+dprintf(STDERR_FILENO, PUSH_FAIL, line_number);
+free_all(1);
 exit(EXIT_FAILURE);
 }
-e_vars.len_stack++;
-return;
-}
-}
-dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+for (i = 0; data.words[1][i]; i++)
+{
+if (isalpha(data.words[1][i]) != 0)
+{
+dprintf(STDERR_FILENO, PUSH_FAIL, line_number);
+free_all(1);
 exit(EXIT_FAILURE);
+}
+}
+num = atoi(data.words[1]);
+if (data.qflag == 0)
+new = add_dnodeint(stack, num);
+else if (data.qflag == 1)
+new = add_dnodeint_end(stack, num);
+if (!new)
+{
+dprintf(STDERR_FILENO, MALLOC_FAIL);
+free_all(1);
+exit(EXIT_FAILURE);
+}
 }
 
 /**
- * _pall - prints all the values on the stack
- * @stack: double pointer to the satack
- * @line_number: line number
- *
- * Return: nothing
+ * pall_handler - handles the pall instruction
+ * @stack: double pointer to the stack to push to
+ * @line_number: number of the line in the file
  */
-void _pall(stack_t **stack, unsigned int line_number)
+void pall_handler(stack_t **stack, unsigned int line_number)
 {
-stack_t *h = *stack;
-(void) line_number;
-for (; h; h = h->next)
-{
-printf("%d\n", h->n);
-}
+(void)line_number;
+if (*stack)
+print_dlistint(*stack);
 }
